a9a690ffd726797a1f0ccc78d72a0f4204b467ba
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-11-23 01:02:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from claseprofile import Profile           
 
 def main():
-    profile1 = Profile(1,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),0)
+    profile1 = Profile(1,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),input("Ingrese su País: "),"online",0)
     profile1.imprimir()
     
-    profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),0)
+    profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),input("Ingrese su País: "),"online",0)
     
     while profile1.password == profile2.password :
         if profile1.password == profile2.password :
             print("CONTRASEÑA INVALIDA, YA EXISTE")
-            profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "))
+            profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),input("Ingrese su País: "),"online",0)
             profile2.imprimir()
 
         else:
