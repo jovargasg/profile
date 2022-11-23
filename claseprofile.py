dc20c5bcf8826962130f3b76fdf2da8f9a31c62c
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-11-22 23:55:11 -0300

    Update claseprofile.py

--- claseprofile.py
+++ claseprofile.py
@@ -6,20 +6,18 @@ class Profile:
     ##fin atributos##
 
     ##metodos##
-    def __init__(self,id,username,email,password,status="online",level=0,country,phrase):
+    def __init__(self,id,username,email,password,status="online",level=0):
         self.id=id
         self.username=username
         self.email=email
         self.password=password
         self.status=status
         self.level=level
-        self.country=country
-        self.phrase=phrase
 
     def imprimir(self):
         print("\nEl id es: ",self.id)
         print("El username es: ",self.username)
         print("El email es: ",self.email)
         print("La contraseña es: ",self.password)
-        print("País: ",self.country)
-        print("Frase: ",self.phrase)
+        print("Frase de perfil: ",self.status)
+        print("Nivel: ",self.level)
