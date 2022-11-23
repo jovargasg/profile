19371b8b5368f66b4fd6c5058854192c78da6c69
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-11-22 23:54:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from claseprofile import Profile           
 
 def main():
-    profile1 = Profile(1,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("¿De que país eres?"),input("Escoge una frase para tu perfil: "))
+    profile1 = Profile(1,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),0)
     profile1.imprimir()
     
-    profile2 = Profile(2,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("¿De que país eres?"),input("Escoge una frase para tu perfil: "))
+    profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "),0)
     
     while profile1.password == profile2.password :
         if profile1.password == profile2.password :
             print("CONTRASEÑA INVALIDA, YA EXISTE")
-            profile2 = Profile(2,input("Nombre de usuario: "),input("Correo: "),input("Contraseña: "),input("¿De que país eres?"),input("Escoge una frase para tu perfil: "))
+            profile2 = Profile(2,input("\nNombre de usuario: "),input("Correo: "),input("Contraseña: "),input("Escoge una frase para tu perfil: "))
             profile2.imprimir()
 
         else:
